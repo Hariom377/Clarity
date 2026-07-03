fa5b99a89a9715332122870794f0b4ab7cbc7444
Hariom377 <hariomdubey377@gmail.com>
2026-07-03 13:01:46 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import { Reveal } from '../components/Reveal';
 import { SectionLabel } from '../components/SectionLabel';
 import ProductMockup from '../components/ProductMockup';
-import SEO from "../components/SEO";
 import { useI18n } from '../lib/i18n';
 import { brand } from '../lib/brand';
 
@@ -19,7 +18,6 @@ export default function Home() {
   const maxAmt = 65000;
 
   return (
-    
     <div>
       {/* ===== HERO ===== */}
       <section className="relative px-5 pt-32 pb-20 sm:px-8 sm:pt-40 sm:pb-28">
